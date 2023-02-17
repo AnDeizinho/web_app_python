63280ff9ad7a13d8cbb3e478e07818564e2ea90e
Andrey Costa de Queiroz-ANDREYCQ <andreycq@motorola.com>
2023-02-16 23:54:54 -0400

    prep hand-on

--- model/contacts.py
+++ model/contacts.py
@@ -11,15 +11,15 @@ class Contact :
         self.email = email
 
 def add(name, fone, email):
-    connection.cursor.execute("insert into tbl_contacts (name, fone, email)  values (?,?,?)", (name, fone, email))
+    connection.cursor.execute(f"insert into tbl_contacts (name, fone, email)  values ('{name}','{fone}','{email}')")
 
 def get_by_id(id):
-    args = connection.cursor.execute("select * from tbl_contacts where id = ?", (id)).fetchone()
+    args = connection.cursor.execute(f"select * from tbl_contacts where id = {id}").fetchone()
     return Contact(*args)
 
 def update(id, name, fone, email):
-    connection.cursor.execute("update tbl_contacts set name = ?, fone = ?, email = ? where id = ?", (name, fone, email, id))
+    connection.cursor.execute(f"update tbl_contacts set name = '{name}', fone = '{fone}', email = '{email}' where id = {id}")
 
 def delete(id):
-    connection.cursor.execute("delete from tbl_contacts where id = ?", (id))
+    connection.cursor.execute("delete from tbl_contacts where id = {id}")
 
